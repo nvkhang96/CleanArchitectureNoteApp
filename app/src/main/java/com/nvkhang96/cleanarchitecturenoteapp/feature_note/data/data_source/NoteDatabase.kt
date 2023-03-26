8a208f56474b8d017f784f7d8aac96484ed9c6ac
Khang <nvkhang96@gmail.com>
2023-03-26 21:19:14 +0700

    Setup Hilt

--- app/src/main/java/com/nvkhang96/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/nvkhang96/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDatabase.kt
@@ -11,4 +11,8 @@ import com.nvkhang96.cleanarchitecturenoteapp.feature_note.domain.model.Note
 abstract class NoteDatabase : RoomDatabase() {
 
     abstract val noteDao: NoteDao
+
+    companion object {
+        const val DATABASE_NAME = "note_db"
+    }
 }
