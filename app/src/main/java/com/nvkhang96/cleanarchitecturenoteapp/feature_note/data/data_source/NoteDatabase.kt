05c2998e169eef90b2cb3eadf53db8b5ec5243ad
Khang <nvkhang96@gmail.com>
2023-03-30 19:43:48 +0700

    Setup navigation

--- app/src/main/java/com/nvkhang96/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/nvkhang96/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDatabase.kt
@@ -7,6 +7,7 @@ import com.nvkhang96.cleanarchitecturenoteapp.feature_note.domain.model.Note
 @Database(
     entities = [Note::class],
     version = 1,
+    exportSchema = false,
 )
 abstract class NoteDatabase : RoomDatabase() {
 
--- app/src/main/java/com/nvkhang96/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/nvkhang96/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreen.kt
@@ -18,10 +18,11 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.nvkhang96.cleanarchitecturenoteapp.feature_note.presentation.notes.components.NoteItem
 import com.nvkhang96.cleanarchitecturenoteapp.feature_note.presentation.notes.components.OrderSection
+import com.nvkhang96.cleanarchitecturenoteapp.feature_note.presentation.util.Screen
 import kotlinx.coroutines.launch
 
 @Composable
-fun NoteScreen(
+fun NotesScreen(
     navController: NavController,
     viewModel: NotesViewModel = hiltViewModel()
 ) {
@@ -33,7 +34,7 @@ fun NoteScreen(
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
-
+                    navController.navigate(Screen.AddEditNoteScreen.route)
                 },
                 backgroundColor = MaterialTheme.colors.primary
             ) {
@@ -81,29 +82,34 @@ fun NoteScreen(
                         viewModel.onEvent(NotesEvent.Order(it))
                     }
                 )
-                Spacer(modifier = Modifier.height(16.dp))
-                LazyColumn(modifier = Modifier.fillMaxSize()) {
-                    items(state.notes) {note ->
-                        NoteItem(
-                            note = note,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .clickable { },
-                            onDeleteClick = {
-                                viewModel.onEvent(NotesEvent.DeleteNode(note))
-                                scope.launch {
-                                    val result = scaffoldState.snackbarHostState.showSnackbar(
-                                        message = "Note deleted",
-                                        actionLabel = "Undo"
-                                    )
-                                    if (result == SnackbarResult.ActionPerformed) {
-                                        viewModel.onEvent(NotesEvent.RestoreNote)
-                                    }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(state.notes) {note ->
+                    NoteItem(
+                        note = note,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                navController.navigate(
+                                    Screen.AddEditNoteScreen.route +
+                                            "?noteId=${note.id}&noteColor=${note.color}"
+                                )
+                            },
+                        onDeleteClick = {
+                            viewModel.onEvent(NotesEvent.DeleteNode(note))
+                            scope.launch {
+                                val result = scaffoldState.snackbarHostState.showSnackbar(
+                                    message = "Note deleted",
+                                    actionLabel = "Undo"
+                                )
+                                if (result == SnackbarResult.ActionPerformed) {
+                                    viewModel.onEvent(NotesEvent.RestoreNote)
                                 }
                             }
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                    }
+                        }
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
                 }
             }
         }
